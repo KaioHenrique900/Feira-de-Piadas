0a08f8710ea421ee895e69c234852bbc2034e453
KaioHenrique900 <kaioneziohenrique@gmail.com>
2021-10-20 11:28:02 -0300

    atualizando

---
+++ app/src/main/java/com/example/kaio/PiadasCategoriaActivity.java
@@ -0,0 +1,14 @@
+package com.example.kaio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PiadasCategoriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piadas_categoria);
+    }
+}
--- app/src/main/java/com/example/kaio/SignUpActivity.java
+++ app/src/main/java/com/example/kaio/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -13,6 +14,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        EditText etDate = findViewById(R.id.etDateUp);
+        etDate.addTextChangedListener(Util.mask(etDate, "##/##/####"));
 
         Button btnCadastrar = findViewById(R.id.btnCadastrarUp);
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
